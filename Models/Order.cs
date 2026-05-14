4f0e406f5779752a6bc5cfffd0b0404520e9cf1b
Aditi <pawarurmila2112@gmail.com>
2026-05-14 16:48:00 +0530

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleOrderService.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime OrderDate { get; set; }
+    }
+}
